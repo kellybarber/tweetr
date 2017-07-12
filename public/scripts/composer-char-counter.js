016eadb103cb9b9548c7a645ac66b12854d446be
kellybarber <kelly.james.barber@gmail.com>
2017-07-12 00:46:57 +0000

    finished css tweet styling

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -3,9 +3,13 @@ $ ("document").ready(() => {
 
   $(".new-tweet form textarea").on("keydown", function(event) {
     $(this).parent().children("span").html(140 - (($(this).val()).length));
+
+
+    if ($(this).parent().children("span").html() < 0) {
+      $(".counter").addClass("negative");
+    } else {
+      $(".counter").removeClass("negative");
+    }
   })
-
-
-
 
 })
