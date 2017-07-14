a2bd92ac29497bde90293c7998c2dd8cfcd6ff80
kellybarber <kelly.james.barber@gmail.com>
2017-07-14 20:48:39 +0000

    Updating before adding mongo as the database

--- mongo_example.js
+++ mongo_example.js
@@ -9,18 +9,60 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
     console.error(`Failed to connect: ${MONGODB_URI}`);
     throw err;
   }
+
+  // We have a connection to the "tweeter" db, starting here.
   console.log(`Connected to mongodb: ${MONGODB_URI}`);
 
-  db.collection("tweets").find().toArray((err, result) => {
+  // ==> Refactored and wrapped as new, tweet-specific function:
+
+  function getTweets(callback) {
+    db.collection("tweets").find().toArray((err, tweets) => {
+      if (err) {
+        return callback(err);
+      }
+      callback(null, tweets);
+    });
+  }
+
+  // ==> Later it can be invoked. Remember even if you pass
+  //     `getTweets` to another scope, it still has closure over
+  //     `db`, so it will still work. Yay!
+
+  getTweets((err, tweets) => {
     if (err) throw err;
 
-    console.log("find result", result);
-    console.log("type of find result: ", typeof result);
+    console.log("Logging each tweet:");
+    for (let tweet of tweets) {
+      console.log(tweet);
+    }
 
     db.close();
-  })
+  });
+
+});
 
 
-
-
-})
+// "use strict";
+//
+// const MongoClient = require("mongodb").MongoClient;
+// const MONGODB_URI = "mongodb://localhost:27017/tweeter";
+//
+// MongoClient.connect(MONGODB_URI, (err, db) => {
+//   if (err) {
+//     console.error(`Failed to connect: ${MONGODB_URI}`);
+//     throw err;
+//   }
+//   console.log(`Connected to mongodb: ${MONGODB_URI}`);
+//
+//   const find = db.collection("tweets").find();
+//
+//   find.toArray((err, tweets) => {
+//     if (err) throw err;
+//
+//     console.log("Results array: ", tweets);
+//
+//     db.close();
+//   })
+//
+//
+// })
