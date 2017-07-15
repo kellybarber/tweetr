0325f307184f3b1c212f5c14f754526fcff794e4
kellybarber <kelly.james.barber@gmail.com>
2017-07-15 03:10:12 +0000

    last merge before cleanup

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -9,18 +9,25 @@ module.exports = function makeDataHelpers(db) {
 
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
-      simulateDelay(() => {
-        db.tweets.push(newTweet);
+      // simulateDelay(() => {
+      db.collection("tweets").insertOne(newTweet, (err, tweets) => {
+        if (err) {
+          return callback(err);
+        }
         callback(null, true);
-      });
+      })
+      // });
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
+      db.collection("tweets").find().toArray((err, tweets) => {
+        if (err) {
+          return callback(err);
+        }
         const sortNewestFirst = (a, b) => b.created_at - a.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
-      });
+        callback(null, tweets.sort(sortNewestFirst));
+      })
     }
 
   };
